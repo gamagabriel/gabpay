a105ca41b69fd84b8a44d5fc97cd3e67f20e06a5
Gabriel Gama <gbr.gama99@gmail.com>
2022-02-26 13:17:10 -0300

    Adição do menu com uso do switch case

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,29 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner ac = new Scanner(System.in);
 
-    }
+		System.out.println("Olá! Bem-vindo ao GabPay!\n");
+		System.out.println("Qual conta você deseja acessar?" + "\n1. Personal Account" + "\n2. Store Account\n");
+		System.out.print("Digite o número correspondente (1/2): ");
+		int num = ac.nextInt();
+
+		switch (num) {
+		case 1:
+			System.out.println("Personal Account");
+			break;
+		case 2:
+			System.out.println("Store Account");
+			break;
+		}
+
+		ac.close();
+	}
+
 }
